725722fedc113c95831f2436cb371518e846a94f
mvwojcik <mateuszw.1998@wp.pl>
2018-12-30 20:11:41 +0100

    Logika działa, nie da się otrzymać błędnego wyniku w żaden sposób Release1.0

--- src/main/java/controllers/MainStageController.java
+++ src/main/java/controllers/MainStageController.java
@@ -16,8 +16,9 @@ public class MainStageController {
     StringProperty firstNumberS = new SimpleStringProperty("0");
     StringProperty secondNumberS = new SimpleStringProperty("0");
     BooleanProperty firstRun = new SimpleBooleanProperty(true);
-    BooleanProperty check = new SimpleBooleanProperty(false);
+    BooleanProperty checkIfDigitHasBeenChosen = new SimpleBooleanProperty(false);
     BooleanProperty isOnNumber = new SimpleBooleanProperty(false);
+    StringProperty lastdigit = new SimpleStringProperty("");
     @FXML
     private Label resultLabel;
     @FXML
@@ -34,15 +35,41 @@ public class MainStageController {
 
     @FXML
     void setDigit(ActionEvent event) {
-        this.digit.setValue(((Button) event.getSource()).getText());
-        this.signLabel.textProperty().bindBidirectional(this.digit);
-        if (!check.getValue()) {
-            this.moveup();
+        if (firstRun.getValue())
+        {
+            this.lastdigit = this.digit;
+            this.digit.setValue(((Button) event.getSource()).getText());
+            this.signLabel.textProperty().bindBidirectional(this.digit);
+            if (isOnNumber.getValue()) {
 
-            check.setValue(true);
+                if (!checkIfDigitHasBeenChosen.getValue()) {
+                    this.moveup();
+
+                    checkIfDigitHasBeenChosen.setValue(true);
+
+                }
+                isOnNumber.setValue(false);
+                this.equalsOnAction2();
+
+            }
         }
-        isOnNumber.setValue(true);
-    }
+        else {
+            if (isOnNumber.getValue()) {
+
+                if (!checkIfDigitHasBeenChosen.getValue()) {
+                    this.moveup();
+
+                    checkIfDigitHasBeenChosen.setValue(true);
+
+                }
+                isOnNumber.setValue(false);
+                this.equalsOnAction2();
+                this.lastdigit = this.digit;
+                this.digit.setValue(((Button) event.getSource()).getText());
+                this.signLabel.textProperty().bindBidirectional(this.digit);
+            }
+        }
+        }
 
 
     @FXML
@@ -52,12 +79,21 @@ public class MainStageController {
 
         System.out.println("First Number:" + firstNumberS.getValue());
         System.out.println("Second number: " + secondNumber.getValue());
-        isOnNumber.setValue(false);
+        isOnNumber.setValue(true);
     }
 
     @FXML
     void equalsOnAction() {
-        getResult();
+        getResult(this.digit);
+        this.firstNumberS.setValue(String.valueOf(this.result1.getValue()));
+        this.firstNumber.setValue(this.result1.getValue());
+        this.clearSecondNumber();
+        firstRun.setValue(false);
+
+    }
+
+    void equalsOnAction2() {
+        getResult(this.lastdigit);
         this.firstNumberS.setValue(String.valueOf(this.result1.getValue()));
         this.firstNumber.setValue(this.result1.getValue());
         this.clearSecondNumber();
@@ -78,8 +114,8 @@ public class MainStageController {
         firstNumber.setValue(Integer.valueOf(firstNumberS.getValue()));
     }
 
-    private void getResult() {
-        char x = this.signLabel.getText().charAt(0);
+    private void getResult(StringProperty digit) {
+        char x = digit.getValue().charAt(0);
         switch (x) {
             case '+':
                 this.result1.setValue(add());
@@ -113,11 +149,14 @@ public class MainStageController {
     }
 
     public int divide() {
-        return firstNumber.getValue() / secondNumber.getValue();
-    }
+        if (secondNumber.getValue()!=0) {
+            return firstNumber.getValue() / secondNumber.getValue();
+        }
+        return firstNumber.getValue();
+        }
 
     @FXML
-    void opposite(ActionEvent event) {
+    void opposite() {
         this.secondNumber.setValue(this.secondNumber.getValue() * -1);
         this.secondNumberS.setValue(String.valueOf(secondNumber.getValue()));
     }
